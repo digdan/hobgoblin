2e73141c986fbb15b46226d04979e9b61e31e446
dmorgan <dmorgan@despina.danmorgan.net>
2013-08-05 11:10:27 -0700

    Added config for auth controller

---
+++ controllers/auth/config.php
@@ -0,0 +1,8 @@
+<?
+return array(
+	'register_redirect'=>'/',
+	'user_table'=>'users',
+	'forgot_token_life'=>360, //How often a requested token can change
+	'forgot_token_size'=>10, //Up to 32
+);
+?>
